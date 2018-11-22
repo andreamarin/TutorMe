80992cc1e3461c401da60a18dada971828de763a
Paola Del Hierro <paoladelhierro@Paolas-MacBook-Pro.local>
2018-11-21 21:49:01 -0600

    carga de materias desde bd

--- templatePagina/search.js
+++ templatePagina/search.js
@@ -34,7 +34,51 @@ function loadTutors(){
 });
 }
 
+function loadMaterias(){
+  var query = db.ref("/deptos/");
+  query.orderByChild("nombre").on('child_added', function(snap){
+    var btn = document.createElement("button");
+    btn.className =   "w3-button w3-block w3-left-align w3-green";
+    btn.id = snap.val().nombre;
+    var id = snap.val().id;
+    btn.innerHTML = snap.val().nombre;
+    bySub.appendChild(btn);
+    addMat(id);
+    btn.onclick = function(){
+      var x = document.getElementById(id);
+      if (x.className.indexOf("w3-show") == -1) {
+          x.className += " w3-show";
+      } else {
+          x.className = x.className.replace(" w3-show", "");
+      }
+    };
+
+    return;
+  });
+
+}
+
+function addMat(nomDep){
+  var divD = document.createElement("div");
+  divD.id = nomDep;
+  divD.className = "w3-bar-block w3-hide";
+  var query = db.ref("/materias/");
+  bySub.appendChild(divD);
+  query.orderByChild("depId").equalTo(nomDep).on('child_added', function(snap){
+      var a = document.createElement("a");
+      a.href = "searchMateria.html";
+      a.className = "w3-bar-item w3-button w3-hover-light-blue";
+      a.style = "padding-left:2%";
+      a.innerHTML = snap.val().nombre;
+      divD.appendChild(a);
+      return;
+  });
+}
+
+
 
 var db = firebase.database();
 const divT = document.getElementById("tutores");
+const bySub = document.getElementById("bySubject");
 loadTutors();
+loadMaterias();
